8aca87288e587966faea7307926753d68923d695
nathcortez <cortez.nathm@gmail.com>
2026-03-31 11:39:33 -0600

    fix: eliminar componente TimeSlots obsoleto

--- src/components/booking/index.ts
+++ src/components/booking/index.ts
@@ -1,6 +1,5 @@
 export { default as BookingFlow } from './BookingFlow';
 export { default as Calendar } from './Calendar';
-export { default as TimeSlots } from './TimeSlots';
 export { default as PetForm } from './PetForm';
 export { default as BookingSummary } from './BookingSummary';
 export { default as PetStep } from './PetStep';
